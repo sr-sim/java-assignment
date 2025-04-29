d3d6fa99ab46c79cb1da54bfa8ab56d414aacbf1
sr-sim <sooruan04@gmail.com>
2025-04-30 01:27:32 +0800

    supplier entry and item entry

--- java-assignment/src/java_assignment2025/FinanceManager.java
+++ java-assignment/src/java_assignment2025/FinanceManager.java
@@ -9,8 +9,8 @@ package java_assignment2025;
  * @author User
  */
 public class FinanceManager extends User{
-    public FinanceManager(String userId, String username, String password) {
-        super(userId, username, password, "Finance Manager");
+    public FinanceManager(String userId, String username, String password, String contact) {
+        super(userId, username, password,contact, "Finance Manager");
     }
     public void managefinance(){
          System.out.println("maange finance ehhh");
--- java-assignment/src/java_assignment2025/User.java
+++ java-assignment/src/java_assignment2025/User.java
@@ -12,12 +12,14 @@ public class User {
     private String userId;
     private String username;
     private String password;
+    private String contact;
     private String role;
     
-    public User(String userId, String username, String password, String role){
+    public User(String userId, String username, String password, String contact, String role){
         this.userId = userId;
         this.username = username;
         this.password = password;
+        this.contact = contact;
         this.role = role;
     }
     public User() {
@@ -46,7 +48,15 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
+    public String getContact(){
+        return contact;
+    }
+    
+    public String setContact(){
+        return contact;
+    }
+    
     public String getRole() {
         return role;
     }
